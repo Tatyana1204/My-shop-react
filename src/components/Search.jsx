ef344091c935f9bb890c325088e4278e66534133
Tatyana1204 <Smetanina2002sme@gmail.com>
2024-09-03 11:25:02 +0300

    test-search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './Search.css';
 
 
-const Search = () => {
+const Search = (props) => {
     return (
         <div className="container">
             <div className="search-box">
-                <input type="text" placeholder="Поиск по объявлениям"/>
+                <input
+                    {...props}
+                    type="text"
+                    placeholder="Поиск по объявлениям"
+                />
                 <button className="btn btn-primary search-btn">
                     <img className="search-btn-icon" src='/images/search.svg' alt="Поиск"/>
                     <span className="search-btn-text">Найти</span>
