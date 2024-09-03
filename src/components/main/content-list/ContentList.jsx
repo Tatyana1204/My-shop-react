611c146e4509abcaf6370c7f4cfa43f9955568fd
Tatyana1204 <Smetanina2002sme@gmail.com>
2024-09-03 16:26:56 +0300

    create cards

--- src/components/main/content-list/ContentList.jsx
+++ src/components/main/content-list/ContentList.jsx
@@ -3,15 +3,20 @@ import './ContentList.css'
 import ContentListItem from "./list-item/Content-list-item";
 
 const ContentList = () => {
+
+    const cards = [
+        {id: 1, img: '/images/cat.jpeg', name: 'Super-cat', price : '10 000 pуб.' , location:'St. Petersburg', date:'03.09.2024' },
+        {id: 2, img: '/images/cat.jpeg', name: 'Super-cat', price : '10 000 pуб.' , location:'St. Petersburg', date:'03.09.2024' },
+        {id: 3, img: '/images/cat.jpeg', name: 'Super-cat', price : '10 000 pуб.' , location:'St. Petersburg', date:'03.09.2024' },
+        {id: 4, img: '/images/cat.jpeg', name: 'Super-cat', price : '10 000 pуб.' , location:'St. Petersburg', date:'03.09.2024' },
+        {id: 5, img: '/images/cat.jpeg', name: 'Super-cat', price : '10 000 pуб.' , location:'St. Petersburg', date:'03.09.2024' },
+        {id: 6, img: '/images/cat.jpeg', name: 'Super-cat', price : '10 000 pуб.' , location:'St. Petersburg', date:'03.09.2024' },
+   ];
     return (
         <div className="content-main_list">
-            <ContentListItem name={'1'}/>
-            <ContentListItem name={'2'}/>
-            <ContentListItem name={'3'}/>
-            <ContentListItem name={'4'}/>
-            <ContentListItem name={'5'}/>
-            <ContentListItem name={'6'}/>
-
+            {cards.map((item, index) =>
+            <ContentListItem props={item}  key={item.id} />
+            )};
         </div>
     );
 };
